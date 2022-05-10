2207b28931d628f10a3eddbc8ca2994f8289de9e
agbarry <aguibou.barry@etu.u-bordeaux.fr>
2022-05-10 21:43:26 +0200

    Maj diagramme

--- src/main/java/dm/al/m1/s2/stock/StockQuotation.java
+++ src/main/java/dm/al/m1/s2/stock/StockQuotation.java
@@ -2,8 +2,8 @@ package dm.al.m1.s2.stock;
 import java.util.ArrayList;
 
 
-
 public interface StockQuotation extends StockValue {
+	
 	boolean hasNext();
 	StockValue next();
 	void reset();
@@ -11,5 +11,4 @@ public interface StockQuotation extends StockValue {
 	ArrayList<StockValue> getTab();
 	// Visitor
 	void accept(Visitor v);
-
 }
